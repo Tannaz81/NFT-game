76b220e962bc953d4318b69a25e1538a88accdf5
tannaz <tannazmhi94@gmail.com>
2022-07-28 15:37:22 +0430

    NFT_game

--- contracts/NFT_game.sol
+++ contracts/NFT_game.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract NftGame is ERC721, Ownable {
     uint256 COUNTER;
 
+    uint256 fee = 0.001 ether;
+
     struct nft {
         string name;
         uint256 id;
@@ -19,25 +21,36 @@ contract NftGame is ERC721, Ownable {
 
     event NewNft(address indexed owner, uint256 id, uint256 dna);
 
-    function getRandomDNA (string memory _str) internal pure returns (uint256) {
-        uint randomNUM = uint256(keccak256(abi.encodePacked(_str)));
-        return randomNUM % 10**16;
+    function createRandomNUM(uint256 _mod) internal view returns (uint256) {
+        uint randomNUM = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender)));
+        return randomNUM % _mod;
+    }
+
+    function updateFee (uint256 _fee) external onlyOwner() {
+        fee = _fee;
+    }
+
+    function withdraw () external payable onlyOwner() {
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
     }
 
    constructor(string memory _name, string memory _symbol)
    ERC721(_name, _symbol) {}
 
-   function createNFT (string memory _name, uint256 _dna) internal {
-    nft memory NFTs = nft(_name, COUNTER, _dna, 1, 50);
+   function createNFT (string memory _name) internal {
+    uint8 randRarity = uint8(createRandomNUM(100));
+    uint randDNA = createRandomNUM(10**16);
+    nft memory NFTs = nft(_name, COUNTER, randDNA, 1, randRarity);
     myNFT.push(NFTs);
     _safeMint(msg.sender, COUNTER);
-    emit NewNft(msg.sender, COUNTER, _dna);
+    emit NewNft(msg.sender, COUNTER, randDNA);
     COUNTER++;
    }
 
-   function createRandomNFT (string memory _name) public {
-    uint randDNA = getRandomDNA(_name);
-    createNFT(_name, randDNA);
+   function createRandomNFT (string memory _name) public payable {
+    require(msg.value == fee, "incorrect fee");
+    createNFT(_name);
    }
 
    function getNFT () public view returns (nft[] memory) {
